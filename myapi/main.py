57730c99bd0c0e2b77dfa877e5a69ad7f2384a31
Diego Alvarez S <dialvarezs@gmail.com>
2022-11-15 17:36:09 -0300

    Added CORS

--- myapi/main.py
+++ myapi/main.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
 
 from myapi.api import router
 from myapi.config import settings
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET", "POST", "PATCH", "OPTIONS"],
+    allow_headers=["*"]
+)
+
 app.mount("/storage", StaticFiles(directory=settings.storage_dir), name="storage")
 
 app.include_router(router=router, prefix="")
